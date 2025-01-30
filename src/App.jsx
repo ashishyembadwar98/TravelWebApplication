30ea01b6b084c41d078034b2e08644a259695f08
Ashish Yembadwar <yembadwara11@gmail.com>
2025-01-30 12:39:52 +0530

    This is Initial Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Hero from './Components/Hero';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+const App = () => {
+  return (
+  <>
+  <Header/>
+  <Hero/>
+  <Footer/>
+  </>
+  )
+};
+
+export default App;
